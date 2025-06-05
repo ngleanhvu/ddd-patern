db3b1f3bf8aa7c875906a097018a8f8928e74c5b
Nguyen Le Anh Vu <vunguyen.0208200402@gmail.com>
2025-06-05 22:43:41 +0700

    update

--- xxx-controller/src/main/java/org/ngleanhvu/ddd/controller/resource/HiController.java
+++ xxx-controller/src/main/java/org/ngleanhvu/ddd/controller/resource/HiController.java
@@ -1,10 +1,15 @@
 package org.ngleanhvu.ddd.controller.resource;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import org.ngleanhvu.ddd.application.service.event.EventApplicationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Random;
 
 @RestController
 @RequestMapping("/hello")
@@ -13,13 +18,32 @@ public class HiController {
     @Autowired
     private EventApplicationService eventApplicationService;
 
+    @RateLimiter(name = "backendA", fallbackMethod = "fallbackHello")
     @GetMapping("/hi")
     public String hi() {
         return eventApplicationService.sayHi("Vu"); 
     }
 
+    public String fallbackHello(Throwable throwable) {
+        return "Too many requests";
+    }
+
+    @RateLimiter(name = "backendB", fallbackMethod = "fallbackHello")
     @GetMapping("/hi/v1")
     public String hiv1() {
         return eventApplicationService.sayHi("Vu");
+    }
+
+    @GetMapping("/circuit/breaker")
+    @CircuitBreaker(name = "checkRandom", fallbackMethod = "fallbackCircuitBreaker")
+    public String circuitBreaker() {
+        RestTemplate restTemplate = new RestTemplate();
+        int productId = new Random().nextInt(20)+1;
+        String url = "https://fakestoreapi.com/products/"+productId;
+        return restTemplate.getForObject(url, String.class);
+    }
+
+    public String fallbackCircuitBreaker(Throwable throwable) {
+        return "Service error";
     }
 }
